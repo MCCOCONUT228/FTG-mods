f8d45a0eb32606c2cf4a99796a5ccd2c3dd2632c
MCCOCONUT228 <46227950+MCCOCONUT228@users.noreply.github.com>
2020-11-06 12:53:34 +0300

    Update average_enjoyeer.py

--- average_enjoyeer.py
+++ average_enjoyeer.py
@@ -7,29 +7,34 @@ import re
 import io
 from textwrap import wrap
 
-ufr = requests.get("https://github.com/LaciaMemeFrame/FTG-Modules/blob/master/open-sans.ttf?raw=true")
-f = ufr.content
+def register(cb):
+	cb(AverageMod())
 
-pic = requests.get("https://raw.githubusercontent.com/MCCOCONUT228/FTG-mods/master/enjoyer.png")
-pic.raw.decode_content = True
-img = Image.open(io.BytesIO(pic.content)).convert("RGB")
-W, H = img.size
+class AverageMod(loader.Module):
+    async def avg(self, message):
+        ufr = requests.get("https://github.com/LaciaMemeFrame/FTG-Modules/blob/master/open-sans.ttf?raw=true")
+        f = ufr.content
 
-text = message.text
-tf = text[4:text.find("&")]
-ts = text[text.find("&")+1:len(text)]
-#t = t.replace("𓃐","\n")
-draw = ImageDraw.Draw(img)
-font = ImageFont.truetype(io.BytesIO(f), 14, encoding='UTF-8')
-imtext = Image.new("RGBA", (W+10, H+10), (0, 0,0,0))
-draw = ImageDraw.Draw(imtext)
-draw.text((10, 10),tf,(0,0,0),font=font, align='left')
-draw.text((340, 10),ts,(0,0,0),font=font, align='left')
-imtext.thumbnail((680, 501))
-w, h = 680, 501
-img.paste(imtext, (10,10), imtext)
-out = io.BytesIO()
-out.name = "enjoyer.jpg"
-img.save(out)
-out.seek(0)
-await message.client.send_file(message.to_id, out, reply_to=reply)
+        pic = requests.get("https://raw.githubusercontent.com/MCCOCONUT228/FTG-mods/master/enjoyer.png")
+        pic.raw.decode_content = True
+        img = Image.open(io.BytesIO(pic.content)).convert("RGB")
+        W, H = img.size
+
+        text = message.text
+        tf = text[4:text.find("&")]
+        ts = text[text.find("&")+1:len(text)]
+        #t = t.replace("𓃐","\n")
+        draw = ImageDraw.Draw(img)
+        font = ImageFont.truetype(io.BytesIO(f), 14, encoding='UTF-8')
+        imtext = Image.new("RGBA", (W+10, H+10), (0, 0,0,0))
+        draw = ImageDraw.Draw(imtext)
+        draw.text((10, 10),tf,(0,0,0),font=font, align='left')
+        draw.text((340, 10),ts,(0,0,0),font=font, align='left')
+        imtext.thumbnail((680, 501))
+        w, h = 680, 501
+        img.paste(imtext, (10,10), imtext)
+        out = io.BytesIO()
+        out.name = "enjoyer.jpg"
+        img.save(out)
+        out.seek(0)
+        await message.client.send_file(message.to_id, out, reply_to=reply)
